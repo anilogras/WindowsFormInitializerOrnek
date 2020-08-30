0b11d49b6a78e7c64cecee1cff39f97dd666f8d3
Anıl OĞRAŞ <forkboysf1@hotmail.com>
2020-08-30 21:03:44 +0300

    Add project files.

---
+++ WindowsFormsApp2/Initializer.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApp2
+{
+    public class Initializer : CreateDatabaseIfNotExists<ProjeContext>
+    {
+        protected override void Seed(ProjeContext context)
+        {
+
+            context.Products.Add(new Entities.Product { Id = 1 , Name = "Deneme" });
+            context.SaveChanges();
+
+            base.Seed(context);
+        }
+    }
+}
---
+++ WindowsFormsApp2/ProjeContext.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WindowsFormsApp2.Entities;
+
+namespace WindowsFormsApp2
+{
+    public class ProjeContext : DbContext
+    {
+
+        public ProjeContext():base("DenemeDb")
+        {
+            Database.SetInitializer<ProjeContext>(new Initializer());
+            Database.Initialize(true); // Normalde bu satırı yazmassan ilk entity sorgunda initializer çalışır ama bunu yazarsan projen çalışınca initializer çalışır ve vten oluşur
+        }
+
+        public DbSet<Product> Products { get; set; }
+
+
+    }
+}
